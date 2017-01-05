33e9b8bfb4a8435bba2e2267356e7bda7812b716
Ali Kaafarani <ali@kvikshaug.no>
2017-01-05 10:30:00 +0100

    feat: separate acl for backups and public content

--- s3-sync/s3-sync.sh
+++ s3-sync/s3-sync.sh
@@ -4,6 +4,7 @@
 source /secrets/aws.env
 
 echo "$(date): Running aws s3 sync"
-aws s3 sync --delete s3://turistforeningen s3://turistforeningen-dev
+aws s3 sync --delete --exclude="*" --include="backups/*" --acl="private" s3://turistforeningen s3://turistforeningen-dev
+aws s3 sync --delete --exclude="backups/*" --acl="public-read" s3://turistforeningen s3://turistforeningen-dev
 STATUS_CODE=$?
 echo "$(date): aws s3 sync exited with status code ${STATUS_CODE}"
